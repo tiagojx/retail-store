88454545c4827b61ba64d66d19698234ebf50bd1
tiagojx <248070259+tiagojx@users.noreply.github.com>
2026-06-02 02:23:43 -0400

    add: postgresql connection

--- backend/internal/database/db.py
+++ backend/internal/database/db.py
@@ -1,61 +1,17 @@
-from decimal import Decimal
-from pydantic import BaseModel, Field
-
-mock_db = [
-    {
-        "name": "hat",
-        "price": Decimal("20"),
-        "cover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpZqAc-BZPyvRq28q5E3pshM5LP6L3vPr7VA&s",
-        "amount": 375,
-    },
-    {
-        "name": "pencil",
-        "price": Decimal("2.89"),
-        "cover": "https://musgravepencil.com/cdn/shop/products/320_Harvest_2048px.jpg?v=1569116504",
-        "amount": 1023,
-    },
-    {
-        "name": "rock in cd",
-        "price": Decimal("899.90"),
-        "cover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpbNMvJgbyrnZyg2P1pGB10kdnqDVVTBJULw&s",
-        "amount": 2,
-    },
-]
+import psycopg
+from typing import Optional
 
 
-class Item(BaseModel):
-    name: str = ""
-    price: Decimal = Field(
-        max_digits=10,
-        decimal_places=2,
-        default=Decimal("0"),
-        json_schema_extra={"examples": [19.99]},
-    )
-    cover: str = ""
-    available: bool = True
+class DB:
+    def __init__(self):
+        self.conn: Optional[psycopg.Connection] = None
 
+    def new_connection(self, _connection_string: str) -> None:
+        if not self.conn or self.conn.closed:
+            self.conn = psycopg.connect(_connection_string)
+            print("Database connection stabilished.")
 
-async def select_item(search_query: str) -> list[Item]:
-    results = []
-    for item in mock_db:
-        if search_query in item["name"]:
-            results.append(
-                Item(name=item["name"], price=item["price"], cover=item["cover"])
-            )
-
-    # if len(results) > 0:
-    # return results
-
-    #    return [Item(available=False)]
-    return results
-
-
-async def get_all_items() -> list[Item]:
-    results = []
-    for item in mock_db:
-        if item["amount"] > 0:
-            results.append(
-                Item(name=item["name"], price=item["price"], cover=item["cover"])
-            )
-
-    return results
+    def disconnect(self):
+        if self.conn and not self.conn.closed:
+            self.conn.close()
+            print("Database connection closed.")
--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,42 @@
+from contextlib import asynccontextmanager
+from dotenv import load_dotenv
+import os
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from .internal.database import db
+from backend.internal.database.db import DB
+from backend.internal.database.sql import SQL
+from backend.internal.handler import item_handler
 
-app = FastAPI()
+
+load_dotenv()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Starting application...")
+    db = DB()
+
+    try:
+        conn_fmt = {"user": os.getenv("DB_USER"), "password": os.getenv("DB_PASSWD")}
+
+        conn_str = "postgresql://{user}:{password}@localhost:5432/main".format(
+            **conn_fmt
+        )
+        db.new_connection(conn_str)
+    except Exception as e:
+        print(f"An error ocurred: {e}")
+        if db.conn:
+            db.conn.rollback()
+
+    yield
+
+    db.disconnect()
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -14,7 +45,7 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def mainpage(request: Request, name: str = "Guest"):
-    results = await db.get_all_items()
+    results = await item_handler.get_all_items()
 
     return templates.TemplateResponse(
         request=request,
@@ -30,7 +61,7 @@ async def mainpage(request: Request, name: str = "Guest"):
 
 @app.get("/search", response_class=HTMLResponse)
 async def search_box(request: Request, query: str):
-    results = await db.select_item(query)
+    results = await item_handler.select_item(query)
 
     return templates.TemplateResponse(
         request=request,
